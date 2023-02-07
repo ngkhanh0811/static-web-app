925f4fc62da3dfa6eef16dd4bca557644385d793
nguyenkhanh08112003 <91303308+nguyenkhanh08112003@users.noreply.github.com>
2023-02-07 09:24:43 +0700

    add function api and create library

--- api/message/index.js
+++ api/message/index.js
@@ -4,10 +4,17 @@ module.exports = async function (context, req) {
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+        : "Test get api";
 
     context.res = {
         // status: 200, /* Defaults to 200 */
+        // cu phap chay api: swa start src --api-location api
         body: responseMessage
     };
+}
+
+module.exports = async function (context, req) {
+    context.res.json({
+        text: "Hello from the API"
+    })
 }
